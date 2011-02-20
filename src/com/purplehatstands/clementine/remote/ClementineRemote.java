ba7c5cca2e373eb22b4a61f1656e5b329ecaccd5
David Sansome <me@davidsansome.com>
2011-02-20 12:55:38 +0000

    Remove the mdns stuff and use libxrme instead

--- src/com/purplehatstands/clementine/remote/ClementineRemote.java
+++ src/com/purplehatstands/clementine/remote/ClementineRemote.java
@@ -1,241 +1,50 @@
 package com.purplehatstands.clementine.remote;
 
-import java.io.IOException;
-import java.net.InetAddress;
-
-import javax.jmdns.JmDNS;
-import javax.jmdns.ServiceEvent;
-import javax.jmdns.ServiceInfo;
-import javax.jmdns.ServiceListener;
-
-import org.jivesoftware.smack.Chat;
-import org.jivesoftware.smack.ChatManager;
-import org.jivesoftware.smack.ConnectionConfiguration;
-import org.jivesoftware.smack.MessageListener;
-import org.jivesoftware.smack.SASLAuthentication;
-import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Presence;
-
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerCallback;
-import android.accounts.AccountManagerFuture;
-import android.accounts.AuthenticatorException;
-import android.accounts.OperationCanceledException;
 import android.app.Activity;
-import android.content.Context;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.graphics.Color;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.net.wifi.WifiManager.MulticastLock;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Gravity;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.os.IBinder;
+import android.widget.Toast;
 
-public class ClementineRemote extends Activity implements ServiceListener {
-  private static final String TAG = "ClementineRemote";
-  static final int ADD_SERVER_REQUEST = 0;
+public class ClementineRemote extends Activity {
+  private RemoteControlService service_;
 
-  private MulticastLock lock_;
-  private JmDNS mdns_ = null;
-  private WifiManager wifi_;
-  private ServerListAdapter servers_;
+  private ServiceConnection connection_ = new ServiceConnection() {
+    public void onServiceConnected(ComponentName className, IBinder service) {
+      service_ = ((RemoteControlService.LocalBinder)service).getService();
 
-  private XMPPConnection xmpp_;
-  
-  private String auth_token_;
-
-  /** Called when the activity is first created. */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.main);
-
-    servers_ = new ServerListAdapter(this);
-
-    ListView listview = (ListView) findViewById(R.id.servers);
-
-    final TextView button = new TextView(this);
-    final Context context = this;
-    button.setText("Add server");
-    button.setHeight(50);
-    button.setTextSize(30);
-    button.setTextColor(Color.WHITE);
-    button.setBackgroundResource(android.R.drawable.list_selector_background);
-    button.setGravity(Gravity.FILL_HORIZONTAL);
-    button.setOnClickListener(new View.OnClickListener() {
-      public void onClick(View v) {
-        Log.d(TAG, "clicked");
-        startActivityForResult(new Intent(context, AddServerActivity.class), ADD_SERVER_REQUEST);
-      }
-    });
-
-    listview.addHeaderView(button);
-    listview.setAdapter(servers_);
-
-    wifi_ = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-    lock_ = wifi_.createMulticastLock("fliing_lock");
-    lock_.setReferenceCounted(true);
-
-
-  }
-
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode != ADD_SERVER_REQUEST) {
-      return;
-    }
-
-    if (resultCode == RESULT_OK) {
-      Bundle extras = data.getExtras();
-      Server server = (Server) extras.get(Server.class.getName());
-      servers_.addServer(server);
-    }
-  }
-
-  @Override
-  public void onResume() {
-    lock_.acquire();
-    if (mdns_ == null) {
-      Log.d(TAG, "Creating MDNS listener");
-      try {
-        WifiInfo info = wifi_.getConnectionInfo();
-        int intaddr = info.getIpAddress();
-
-        byte[] byteaddr = new byte[] { (byte)(intaddr & 0xff), (byte)(intaddr >> 8 & 0xff), (byte)(intaddr >> 16 & 0xff), (byte)(intaddr >> 24 & 0xff) };
-        InetAddress addr = InetAddress.getByAddress(byteaddr);
-
-        Log.d(TAG, String.format("found intaddr=%d, addr=%s", intaddr, addr.toString()));
-
-        mdns_ = JmDNS.create(addr, "foobar");
-        mdns_.addServiceListener("_clementine._tcp.local.", this);
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-    }
-    
-    AccountManager manager = AccountManager.get(this);
-    Account google_account = manager.getAccountsByType("com.google")[0];
-    manager.getAuthToken(google_account, "mail", true, new AccountManagerCallback<Bundle>() {
-      public void run(AccountManagerFuture<Bundle> arg0) {
-        try {
-          Bundle bundle = arg0.getResult();
-          if (bundle.containsKey("authtoken")) {
-            auth_token_ = bundle.getString("authtoken");
-            Log.d(TAG, "Auth token:" + auth_token_);
-            Connect();
-          } else if (bundle.containsKey(AccountManager.KEY_INTENT)) {
-            Intent intent = (Intent)bundle.get(AccountManager.KEY_INTENT);
-            startActivity(intent);
-          }
-        } catch (OperationCanceledException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        } catch (AuthenticatorException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        } catch (IOException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
+      service_.AddConnectionHandler(new RemoteControlService.ConnectionHandler() {
+        public void OnDisconnected() {
+          Toast.makeText(ClementineRemote.this, "Disconnected from XMPP", Toast.LENGTH_LONG).show();
         }
         
-      }
-    }, null);
-
-
-    super.onResume();
-  }
-  
-  private void Connect() {
-    SASLAuthentication.registerSASLMechanism("X-GOOGLE-TOKEN", GoogleTokenAuthenticator.class);
-    
-    
-    SASLAuthentication.supportSASLMechanism("X-GOOGLE-TOKEN", 0);
-    // X-GOOGLE-TOKEN service=mail
-    // <0×00>tokenmechanism@gmail.com<0×00>DQAAAHIA … QhXLa4g
-    // base64: AHRva2VubWVjaGFu ..snip snip.. JS2RSVzE1aXlvZEMtZmpTUWhYTGE0Zw==
-    ConnectionConfiguration config = new ConnectionConfiguration("talk.google.com", 5222, "gmail.com");
-    config.setDebuggerEnabled(true);
-    xmpp_ = new XMPPConnection(config);
-    try {
-      xmpp_.connect();
-      xmpp_.login("john.maguire@gmail.com", auth_token_);
-      Presence presence = new Presence(Presence.Type.available);
-      presence.setStatus("Hello World!");
-      xmpp_.sendPacket(presence);
-    } catch (XMPPException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+        public void OnConnectionFailure(String message) {
+          Toast.makeText(ClementineRemote.this, "Failed to connect to XMPP: " + message, Toast.LENGTH_LONG).show();
+        }
+        
+        public void OnConnected() {
+          Intent intent = new Intent(ClementineRemote.this, NowPlayingActivity.class);
+          ClementineRemote.this.startActivity(intent);
+        }
+      });
+      
+      service_.Connect(ClementineRemote.this);
     }
-  }
-  
-  private void SendMessage() {
-    ChatManager chat_manager = xmpp_.getChatManager();
-    Chat chat = chat_manager.createChat("john.maguire@gmail.com/foobar1A6235CD", new MessageListener() {
-      public void processMessage(Chat chat, Message message) {
-        Log.d(TAG, "Received message:" + message.getBody() + " from:" + message.getFrom());
-      }
-    });
 
-    try {
-      Message message = new Message();
-      message.setTo("john.maguire@gmail.com/foobar1A6235CD");
-      message.addBody("en", "Lo World!");
-      Log.d(TAG, message.toXML());
-      chat.sendMessage(message);
-    } catch (XMPPException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+    public void onServiceDisconnected(ComponentName className) {
     }
+  };
+  
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    
+    bindService(new Intent(this, RemoteControlService.class), connection_, BIND_AUTO_CREATE);
   }
 
-  @Override
-  public void onPause() {
-    lock_.release();
-    super.onPause();
-  }
-
-  @Override
-  public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menu_info) {
-    super.onCreateContextMenu(menu, v, menu_info);
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.server_context, menu);
-  }
-
-  @Override
-  public boolean onContextItemSelected(MenuItem item) {
-    // TODO: Implement remove.
-    return true;
-  }
-
-
-  public void serviceAdded(ServiceEvent event) {
-    Log.d(TAG, event.getName());
-    // Lookup full TXT record.
-    ServiceInfo info = mdns_.getServiceInfo("_clementine._tcp.local.", event.getName());
-    Log.d(TAG, info.toString());
-
-    String address = info.getHostAddress();
-    int port = info.getPort();
-
-    servers_.addDetectedServer(new Server(address, address, port));
-  }
-
-  public void serviceRemoved(ServiceEvent event) {
-    // TODO Auto-generated method stub
-  }
-
-  public void serviceResolved(ServiceEvent event) {
-    // TODO Auto-generated method stub
+  protected void onDestroy() {
+    unbindService(connection_);
+    super.onDestroy();
   }
 }
---
+++ src/com/purplehatstands/clementine/remote/RemoteControlService.java
@@ -0,0 +1,158 @@
+package com.purplehatstands.clementine.remote;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.XMPPException;
+
+import com.purplehatstands.libxrme.Connection;
+import com.purplehatstands.libxrme.RemoteControlInterface;
+import com.purplehatstands.libxrme.State;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+public class RemoteControlService extends Service {
+  private final IBinder binder_ = new LocalBinder();
+  
+  private Context ui_context_;
+  private String auth_token_;
+  private Account google_account_;
+  private Connection connection_;
+  private boolean first_connection_attempt_ = true;
+  
+  private List<ConnectionHandler> connection_handlers_ = new ArrayList<ConnectionHandler>();
+  private List<MediaStateHandler> media_state_handlers_ = new ArrayList<MediaStateHandler>();
+  
+  public class LocalBinder extends Binder {
+    RemoteControlService getService() {
+      return RemoteControlService.this;
+    }
+  }
+  
+  public interface ConnectionHandler {
+    public void OnConnected();
+    public void OnConnectionFailure(String message);
+    public void OnDisconnected();
+  }
+  
+  public interface MediaStateHandler {
+    public void OnStateChanged(State state);
+    public void OnAlbumArtChanged(Bitmap image);
+  }
+
+  public void AddConnectionHandler(ConnectionHandler handler) {
+    connection_handlers_.add(handler);
+  }
+  
+  public void AddMediaStateHandler(MediaStateHandler handler) {
+    media_state_handlers_.add(handler);
+  }
+
+  public IBinder onBind(Intent intent) {
+    return binder_;
+  }
+  
+  public void Connect(Context ui_context) {
+    ui_context_ = ui_context;
+    
+    GetAuthToken();
+  }
+  
+  private void GetAuthToken() {
+    AccountManager manager = AccountManager.get(this);
+    google_account_ = manager.getAccountsByType("com.google")[0];
+    
+    manager.getAuthToken(google_account_, "talk", true, new AccountManagerCallback<Bundle>() {
+      public void run(AccountManagerFuture<Bundle> arg0) {
+        try {
+          Bundle bundle = arg0.getResult();
+          if (bundle.containsKey("authtoken")) {
+            auth_token_ = bundle.getString("authtoken");
+            StartConnection();
+          } else if (bundle.containsKey(AccountManager.KEY_INTENT)) {
+            Intent intent = (Intent)bundle.get(AccountManager.KEY_INTENT);
+            ui_context_.startActivity(intent);
+          }
+        } catch (OperationCanceledException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (AuthenticatorException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        
+      }
+    }, null);
+  }
+  
+  private void StartConnection() {
+    SASLAuthentication.registerSASLMechanism("X-GOOGLE-TOKEN", GoogleTokenAuthenticator.class);
+    SASLAuthentication.supportSASLMechanism("X-GOOGLE-TOKEN", 0);
+    
+    connection_ = new Connection();
+    connection_.set_username(google_account_.name);
+    connection_.set_password(auth_token_);
+    connection_.set_agent_name("Clementine Remote on " + Build.PRODUCT);
+    
+    connection_.SetRemoteControl(new RemoteControlInterface() {
+      public void StateChanged(String peer_jid_resource, State state) {
+        for (MediaStateHandler handler: media_state_handlers_) {
+          handler.OnStateChanged(state);
+        }
+      }
+      
+      public void AlbumArtChanged(String peer_jid_resource, Bitmap image) {
+        for (MediaStateHandler handler: media_state_handlers_) {
+          handler.OnAlbumArtChanged(image);
+        }
+      }
+    });
+    
+    try {
+      connection_.Connect();
+    } catch (XMPPException e) {
+      HandleConnectionFailure(e.getMessage());
+      return;
+    }
+    
+    for (ConnectionHandler handler: connection_handlers_) {
+      handler.OnConnected();
+    }
+  }
+
+  private void HandleConnectionFailure(String message) {
+    if (first_connection_attempt_) {
+      // Try invalidating the auth token and trying to connect again
+      AccountManager manager = AccountManager.get(this);
+      manager.invalidateAuthToken("com.google", auth_token_);
+      
+      first_connection_attempt_ = false;
+      GetAuthToken();
+    } else {
+      for (ConnectionHandler handler: connection_handlers_) {
+        handler.OnConnectionFailure(message);
+      }
+    }
+  }
+}
